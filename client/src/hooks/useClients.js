832ae32f0ad052794c96da46d53cd4e8897b8dbf
Ninkov <ninkov82@gmail.com>
2024-08-15 17:50:42 +0300

    implement create

--- client/src/hooks/useClients.js
+++ client/src/hooks/useClients.js
@@ -2,28 +2,31 @@ import { useEffect, useState } from "react";
 
 import clientsAPI from "../api/clients-api";
 
-export function useGetAllClients(){
-    const [clients, setClients] = useState([]);
-    useEffect(() => {
-      (async () => {
-        const result = await clientsAPI.getAll();
-        setClients(result);
-      })();
-    }, []);
+export function useGetAllClients() {
+  const [clients, setClients] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const result = await clientsAPI.getAll();
+      setClients(result);
+    })();
+  }, []);
 
-
-    return [clients, setClients]
+  return [clients, setClients];
 }
-export function useGetOneClients(clientsId){
-
+export function useGetOneClients(clientsId) {
   const [client, setClient] = useState({});
 
-    useEffect(() => {
-        (async () => {
-          const result = await clientsAPI.getOne(clientsId);
-          setClient(result);
-        })();
-      }, [clientsId]);
-      return [client, setClient]
+  useEffect(() => {
+    (async () => {
+      const result = await clientsAPI.getOne(clientsId);
+      setClient(result);
+    })();
+  }, [clientsId]);
+  return [client, setClient];
+}
 
+export function useCreateClient() {
+  const clientCreateHandler = (clientData) => clientsAPI.create(clientData);
+
+  return clientCreateHandler;
 }
