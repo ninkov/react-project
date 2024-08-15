40639d66f299314e861161c5465c505462a24bda
Ninkov <ninkov82@gmail.com>
2024-08-15 13:39:51 +0300

    Remove saved pass from auth state

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -6,9 +6,9 @@ export const useLogin = () => {
   const { changeAuthState } = useContext(AuthContext);
 
   const loginHandler = async (email, password) => {
-    const result = await login(email, password);
-    changeAuthState(result);
-    return result;
+    const { password:_,...authData } = await login(email, password);
+    changeAuthState(authData);
+    return authData;
   };
   return loginHandler;
 };
@@ -17,7 +17,7 @@ export const useRegister = () => {
   const { changeAuthState } = useContext(AuthContext);
 
   const registerHandler = async (email, password) => {
-    const { ...authData } = await register(email, password);
+    const { password:_,...authData } = await register(email, password);
 
     changeAuthState(authData);
 
