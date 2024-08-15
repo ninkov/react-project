1ad970b262b93838a4f2d99eb1c7b8d35cbf305b
Ninkov <ninkov82@gmail.com>
2024-08-15 17:42:03 +0300

    change BASE_URL

--- client/src/api/clients-api.js
+++ client/src/api/clients-api.js
@@ -1,19 +1,22 @@
-import  request from "./requester";
+import request from "./requester";
 
-const BASE_URL = `http://localhost:3030/jsonstore/clients`;
+const BASE_URL = `http://localhost:3030/data/clients`;
 
-export const getAll = async () => {
+ export const getAll = async () => {
   const result = await request.get(BASE_URL);
 
   const clients = Object.values(result);
   return clients;
 };
 
-export const getOne = (clientId) => request.get(`${BASE_URL}/${clientId}`);
+ export const getOne = (clientId) => request.get(`${BASE_URL}/${clientId}`);
+
+ export const create = (clientData) => request.post(`${BASE_URL}`, clientData);
 
 const clientsAPI = {
   getOne,
   getAll,
+  create,
 };
 
 export default clientsAPI;
